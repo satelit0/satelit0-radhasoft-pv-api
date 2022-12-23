f1e9dffdde275eaec6bc6551b147cba49a502cfa
satelit0 <satelite0ster@gmail.com>
2022-12-23 08:58:41 -0400

    add enum postgres error code

--- src/helpers/enums.ts
+++ src/helpers/enums.ts
@@ -51,3 +51,13 @@ export enum WeekendCorrection {
   AFTER =  2, //"Despues",
   DURING = 0, //"Durante" 
 }
+
+export enum PostgresErrorCode {
+  INTEGRITY_CONSTRAINT_VIOLATION = '23000',
+  RESTRICT_VIOLATION = '23001',
+  NOT_NULL_VIOLATION = '23502',
+  FOREIGN_KEY_VIOLATION = '23503',
+  UNIQUE_VIOLATION = '23505',
+  CHECK_VIOLATION = '23514',
+  EXCLUSION_VIOLATION = '23P01',
+}
